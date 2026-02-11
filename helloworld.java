c744b4d71b9a40cf70f600b8abee76c09e6f765e
Rahul Girdhar <girdhar.mphasis@gmail.com>
2026-02-11 21:37:03 +0530

    Add welcome message for Shoni's workshop

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Welcome to DevOps workshop");
+        System.out.println("Welcome to Shoni's workshop");
     }
 }
